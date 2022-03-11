59630606542371d1257b3d74dd310f7153c94865
Leon Si <contact@leonzalion.com>
2022-03-11 00:01:42 -0500

    test: broken tests

--- test/suites/prompt.test.ts
+++ test/suites/prompt.test.ts
@@ -1,5 +1,175 @@
-import { test } from 'vitest';
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 
-test('my ', () => {
-	// Your tests go here
+import type { Question, QuestionCollection } from 'inquirer';
+import inquirer from 'inquirer';
+import { describe, it, expect } from 'vitest';
+import { ReadlineStub } from '~test/utils/readline.js';
+import SuperCheckboxPrompt from '~/index.js';
+
+describe('inquirer-autocomplete-prompt', () => {
+	describe('Pass a choices', () => {
+		const createPrompt = (questions: QuestionCollection) => {
+			const rl = new ReadlineStub();
+			const prompt = new SuperCheckboxPrompt(questions as Question, rl, []);
+			return { prompt, rl };
+		};
+
+		const defaultSource = ['foo', new inquirer.Separator(), 'bar', 'baz'];
+
+		it('applies choices', async () => {
+			const { prompt, rl } = createPrompt({
+				message: 'test',
+				name: 'name',
+				source: defaultSource,
+			});
+
+			const run = prompt.run();
+
+			rl.moveDown();
+			rl.moveDown();
+			rl.space();
+			rl.enter();
+
+			const answer = await run;
+
+			expect(answer).to.have.lengthOf(1).to.include.members(['bar']);
+		});
+
+		it('applies choices with default', async () => {
+			const { prompt, rl } = createPrompt({
+				message: 'test',
+				name: 'name',
+				source: defaultSource,
+				default: ['bar'],
+			});
+
+			const run = prompt.run();
+			rl.space();
+			rl.enter();
+
+			const answer = await run;
+			expect(answer).to.have.lengthOf(2).to.include.members(['foo', 'bar']);
+		});
+
+		it('applies validate', async () => {
+			const { prompt, rl } = createPrompt({
+				message: 'test',
+				name: 'name',
+				source: defaultSource,
+				validate(answer) {
+					if (answer.length === 0) {
+						return 'You must choose at least one topping.';
+					} else {
+						return true;
+					}
+				},
+			});
+
+			const run = prompt.run();
+			rl.enter();
+			rl.space();
+			rl.enter();
+
+			const answer = await run;
+			expect(answer).to.have.lengthOf(1).to.include.members(['foo']);
+		});
+
+		it('searching', async () => {
+			const { prompt, rl } = createPrompt({
+				message: 'test',
+				name: 'name',
+				source: defaultSource,
+			});
+			const run = prompt.run();
+
+			rl.type('um');
+			rl.space();
+
+			rl.line = '';
+
+			rl.type('ba');
+			rl.space();
+			rl.enter();
+
+			const answer = await run;
+
+			expect(answer).to.have.lengthOf(2).to.include.members(['bum', 'bar']);
+		});
+
+		it('when tab pressed', async () => {
+			const { prompt, rl } = createPrompt({
+				message: 'test',
+				name: 'name',
+				source: defaultSource,
+			});
+
+			const run = prompt.run();
+
+			rl.tab();
+			rl.space();
+			rl.enter();
+
+			const answer = await run;
+			expect(answer).to.have.lengthOf(1).to.include.members(['foo']);
+		});
+
+		it('cancel default value', async () => {
+			const { prompt, rl } = createPrompt({
+				message: 'test',
+				name: 'name',
+				source: defaultSource,
+				default: ['bar'],
+			});
+
+			const run = prompt.run();
+
+			rl.type('ba');
+			rl.space();
+			rl.enter();
+
+			const answer = await run;
+
+			expect(answer).to.be.an('array').to.have.lengthOf(0);
+		});
+
+		it('loops choices going down', async () => {
+			const { prompt, rl } = createPrompt({
+				message: 'test',
+				name: 'name',
+				source: ['foo', new inquirer.Separator(), 'bar', 'bum'],
+			});
+
+			const run = prompt.run();
+
+			rl.moveDown();
+			rl.moveDown();
+			rl.moveDown();
+			rl.moveDown();
+			rl.space();
+			rl.enter();
+
+			const answer = await run;
+
+			expect(answer).to.have.lengthOf(1).to.include.members(['foo']);
+		});
+
+		it('requires a name', () => {
+			expect(() => {
+				createPrompt({
+					message: 'foo',
+					source: defaultSource,
+				});
+			}).to.throw(/name/);
+		});
+
+		it('requires source', () => {
+			expect(() => {
+				createPrompt({
+					name: 'foo',
+					message: 'foo',
+				});
+			}).to.throw(/choices/);
+		});
+	});
+	// TODO: add tests where source is a function
 });
---
+++ test/utils/readline.ts
@@ -0,0 +1,80 @@
+import type { EventEmitter } from 'node:events';
+import { Interface } from 'node:readline';
+import { Duplex } from 'node:stream';
+import { vi } from 'vitest';
+
+export class ReadlineStub extends Interface {
+	line: string;
+	input: EventEmitter;
+
+	constructor() {
+		const input = new Duplex({ read() {} });
+		super(input);
+
+		this.line = '';
+		this.input = input;
+
+		Object.assign(this, {
+			write: vi.fn().mockReturnThis(),
+			moveCursor: vi.fn().mockReturnThis(),
+			setPrompt: vi.fn().mockReturnThis(),
+			close: vi.fn().mockReturnThis(),
+			pause: vi.fn().mockReturnThis(),
+			resume: vi.fn().mockReturnThis(),
+			_getCursorPos: vi.fn().mockReturnValue({
+				cols: 0,
+				rows: 0,
+			}),
+			output: {
+				end: vi.fn().mockReturnThis(),
+				mute: vi.fn().mockReturnThis(),
+				unmute: vi.fn().mockReturnThis(),
+				__raw__: '',
+				write(str: string) {
+					this.__raw__ += str;
+				},
+			},
+		});
+	}
+
+	type(word: string) {
+		for (const char of word) {
+			this.line += char;
+			this.input.emit('keypress', char);
+		}
+	}
+
+	moveDown() {
+		this.input.emit('keypress', '', {
+			name: 'down',
+		});
+	}
+
+	moveUp() {
+		this.input.emit('keypress', '', {
+			name: 'up',
+		});
+	}
+
+	enter() {
+		this.emit('line');
+	}
+
+	space() {
+		this.input.emit('keypress', '', {
+			name: 'space',
+		});
+	}
+
+	tab() {
+		this.input.emit('keypress', '', {
+			name: 'tab',
+		});
+	}
+
+	typeNonChar() {
+		this.input.emit('keypress', '', {
+			name: 'shift',
+		});
+	}
+}
